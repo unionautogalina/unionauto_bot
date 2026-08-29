bdf0fc5d340ab0c3d6b52d59074e7ee69cb4464d
unionautogalina <321642378+unionautogalina@users.noreply.github.com>
2026-08-29 23:38:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,41 @@
+# -*- coding: utf-8 -*-
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 BOT_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
+ADMIN_IDS = [7838436130]
+
+PHONE = "8 800 600-79-66"
+PHONE_TEL = "88006007966"
+MOBILE = "8 951 006 80-50"
+MOBILE_TEL = "79510068050"
+ADMIN_USERNAME = "UnionAutoAdmin"
+EMAIL = "unionauto.org@mail.ru"
+WHATSAPP = "https://wa.me/79510068050"
+MAX_MESSENGER = "https://max.ru/join/Wvg0aUcpMg0A7T9NCQal4-j3rLlXByxLx-fKTQgEcMY"
+
+CITY = "Владивосток"
+ADDRESS = "г. Владивосток, ул. Сипягина 20б, офис 301"
+MAP_2GIS = "https://2gis.ru/vladivostok/geo/70000001066528297"
+WORK_HOURS = "Пн–Пт 10:00–17:00 (по Владивостоку)"
+
+SITE = "https://unionauto.org"
+SITE_STOCK = "https://unionauto.org/stock-auto/"
+SITE_BROUGHT = "https://unionauto.org/experience/"
+SITE_REVIEWS = "https://unionauto.org/reviews/"
+SITE_NEWS = "https://unionauto.org/articles/"
+TG_CHANNEL = "https://t.me/unionautorussia"
+TG_HASHTAG_STOCK = "https://t.me/unionautorussia?q=%23%D0%B2%D0%BD%D0%B0%D0%BB%D0%B8%D1%87%D0%B8%D0%B8"
+
+INSTAGRAM = "https://www.instagram.com/unionauto_org"
+YOUTUBE = "https://youtube.com/@unionauto125"
+VK = "https://vk.com/unionauto_org"
+RUTUBE = "https://rutube.ru/channel/45242826"
+
 YEARS = 12
 SINCE = 2014
-CLIENTS = 10_000
-UNITS_LAST_YEAR = 170
+CLIENTS = "10 000+"
+UNITS_LAST_YEAR = "170+"
 BANKS = 20
-
-ADMIN_USERNAME = "unionautoadmin"
-PHONE = "8(800)600-79-66"
-MOBILE = "8 951 006-80-50"
-ADDRESS = "г. Владивосток, ул. Сипягина 20б, офис 301"
-WORK_HOURS = "с 10:00 до 17:00"
-EMAIL = "unionauto.org@mail.ru"
