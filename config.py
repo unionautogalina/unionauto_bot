8cc670077457e259f710318368c898fdd064dc71
unionautogalina <321642378+unionautogalina@users.noreply.github.com>
2026-08-29 23:27:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
 import os
 
 BOT_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+
 YEARS = 12
 SINCE = 2014
+CLIENTS = 10_000
+UNITS_LAST_YEAR = 170
+BANKS = 20
+
+ADMIN_USERNAME = "unionautoadmin"
+PHONE = "8(800)600-79-66"
+MOBILE = "8 951 006-80-50"
+ADDRESS = "г. Владивосток, ул. Сипягина 20б, офис 301"
+WORK_HOURS = "с 10:00 до 17:00"
+EMAIL = "unionauto.org@mail.ru"
